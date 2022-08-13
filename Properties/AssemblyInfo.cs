c6098a838349c18f69da4eec098cf304eb01daf0
David Geary <david@secondbounce.com>
2022-08-13 15:01:31 +0100

    Add test project

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GhActionsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b935ee34-bd88-46d5-9a36-bb800245a988")]
